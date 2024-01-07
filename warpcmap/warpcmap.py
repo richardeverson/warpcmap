cfdfefd1282252ef6b98df9f7ac61d5ef39f1e2d
Richard Everson <R.M.Everson@exeter.ac.uk>
2024-01-07 17:59:01 +0000

    Check for legal z and clean up white space.

--- warpcmap/warpcmap.py
+++ warpcmap/warpcmap.py
@@ -7,44 +7,44 @@ from scipy.optimize import root_scalar
 
 def warp_colormap(basemap, z, beta=1, Nentries=256):
     """
-    Construct a new colormap by warping basemap so that the colour 
-    in "middle" of the basemap is (ie, corresponding to a value of 0.5) 
-    corresponds to the value z and the rate of change of colours around z 
+    Construct a new colormap by warping basemap so that the colour
+    in "middle" of the basemap is (ie, corresponding to a value of 0.5)
+    corresponds to the value z and the rate of change of colours around z
     is given by beta.
-    
+
     Parameters
     ----------
-    
+
     basemap:  Matplotlib ColorMap or string naming one.
         The colormap to warp
-    
+
     z:  float (0 < z < 1)
 
-        The location that the middle of the basemap is warped to. 
-        For instance, if the basemap is the 'jet' colourmap, so that 
-        the middle of the map is green, then in the new colourmap, 
-        green corresponds to the value z. 
-        
+        The location that the middle of the basemap is warped to.
+        For instance, if the basemap is the 'jet' colourmap, so that
+        the middle of the map is green, then in the new colourmap,
+        green corresponds to the value z.
+
     beta: float (beta > 0)
         Beta controls the rate of change of colours close to z.  It is
-        approximately the gradient of the mapping between the new colours 
-        and the old colours.  Values of beta in the range 1 to 5 are usual 
-        as they give more resolution to the data values close to z.  
+        approximately the gradient of the mapping between the new colours
+        and the old colours.  Values of beta in the range 1 to 5 are usual
+        as they give more resolution to the data values close to z.
         beta < 1 compresses the colours close to z.
-        
+
     Nentries: int
         Number of entries in the new colourmap.
-        
+
     Returns
     -------
-    
+
     newmap:  ListedColormap
         The warped colourmap.
-        
-        
+
+
     Example
     -------
-    
+
     Emphasise the data values around 0.8
     >>> cmap = warp_colormap('jet', z=0.8, beta=3)
     >>> imshow(X, cmap=cmap)
@@ -52,20 +52,22 @@ def warp_colormap(basemap, z, beta=1, Nentries=256):
 
     Note
     ----
-    
-    If the range of the data is not [0, 1], then z is linearly mapped 
-    to the data range so if the data range is (100, 120) and z = 0.8, 
-    then the emphasised values will be around 116.    
+
+    If the range of the data is not [0, 1], then z is linearly mapped
+    to the data range so if the data range is (100, 120) and z = 0.8,
+    then the emphasised values will be around 116.
     """
+    if not (0 < z < 1):
+        raise ValueError('z must be between 0 and 1')
     if isinstance(basemap, str):
         basemap = cm.get_cmap(basemap)
-        
+
     def objective(alpha):
         return betainc(alpha, beta, z) - 0.5
 
     soln = root_scalar(objective, bracket=(1e-10, 1e2))
     alpha = soln.root
-    
+
     rgba = np.zeros((Nentries, 4))
     for n, y in enumerate(np.linspace(0, 1, Nentries)):
         x = betainc(alpha, beta, y)
@@ -87,15 +89,15 @@ def wimshow(X,
     ----------
     X : array-like
         The image with scalar data.  The data is visualized
-        using a colormap. If RGB or RGBA data is to be displayed, just 
-        use `imshow` directly. 
+        using a colormap. If RGB or RGBA data is to be displayed, just
+        use `imshow` directly.
 
         The two dimensions (M, N) define the rows and columns of
         the image.
 
     cmap : str or `~matplotlib.colors.Colormap`, optional
         The Colormap instance or registered colormap name used to map
-        scalar data to colors. 
+        scalar data to colors.
         Defaults to :rc:`image.cmap`.
 
     vmin, vmax : scalar, optional
@@ -105,25 +107,25 @@ def wimshow(X,
         data. *vmin*, *vmax* are ignored if the *norm* parameter is used.
 
     vmid: scalar, optional
-        The data value that the middle of the colormap is warped to. 
-        For instance, if the colormap is the 'jet' colourmap, so that 
-        the middle of the map is green, then in the new colourmap, 
+        The data value that the middle of the colormap is warped to.
+        For instance, if the colormap is the 'jet' colourmap, so that
+        the middle of the map is green, then in the new colourmap,
         green corresponds to the value vmid.
         Default is the middle of the range of the data.
 
     beta:  scalar > 0, optional
-        Beta controls the rate of change of colours close to *vmid*.  
+        Beta controls the rate of change of colours close to *vmid*.
         Larger values of beta give a more rapid change of colour with
-        data value.  Values of beta in the range 1 to 5 are usual 
-        as they give more resolution to the data values close to *vmid*.  
+        data value.  Values of beta in the range 1 to 5 are usual
+        as they give more resolution to the data values close to *vmid*.
         beta < 1 compresses the colours close to *vmid*.
-    
+
     Nentries: int, optional
         Number of entries in the warped colourmap.
         Default: 256
 
     ax: matplotlib.axes.Axes, optional
-        The Matplotlib axes in which to plot. 
+        The Matplotlib axes in which to plot.
         Default:  The current axes.
 
     Returns
@@ -178,12 +180,12 @@ def wpcolormesh(*args,
         The values to be color-mapped.
 
     X, Y : array-like, optional
-        The coordinates of the corners of quadrilaterals of a pcolormesh.  
-        See `pcolormesh` for details. 
+        The coordinates of the corners of quadrilaterals of a pcolormesh.
+        See `pcolormesh` for details.
 
     cmap : str or `~matplotlib.colors.Colormap`, optional
         The Colormap instance or registered colormap name used to map
-        scalar data to colors. 
+        scalar data to colors.
         Defaults to :rc:`image.cmap`.
 
     vmin, vmax : scalar, optional
@@ -193,26 +195,26 @@ def wpcolormesh(*args,
         data. *vmin*, *vmax* are ignored if the *norm* parameter is used.
 
     vmid: scalar, optional
-        The data value that the middle of the colormap is warped to. 
-        For instance, if the colormap is the 'jet' colourmap, so that 
-        the middle of the map is green, then in the new colourmap, 
+        The data value that the middle of the colormap is warped to.
+        For instance, if the colormap is the 'jet' colourmap, so that
+        the middle of the map is green, then in the new colourmap,
         green corresponds to the value vmid.
-        Default is the middle of the range of the data or (*vmin* + *vmax*)/2 
+        Default is the middle of the range of the data or (*vmin* + *vmax*)/2
         if they are specified.
 
     beta:  scalar > 0, optional
-        Beta controls the rate of change of colours close to *vmid*.  
+        Beta controls the rate of change of colours close to *vmid*.
         Larger values of beta give a more rapid change of colour with
-        data value.  Values of beta in the range 1 to 5 are usual 
-        as they give more resolution to the data values close to *vmid*.  
+        data value.  Values of beta in the range 1 to 5 are usual
+        as they give more resolution to the data values close to *vmid*.
         beta < 1 compresses the colours close to *vmid*.
-    
+
     Nentries: int, optional
         Number of entries in the warped colourmap.
         Default: 256
 
     ax: matplotlib.axes.Axes, optional
-        The Matplotlib axes in which to plot. 
+        The Matplotlib axes in which to plot.
         Default:  The current axes.
 
     Returns
@@ -274,12 +276,12 @@ def wpcolor(*args,
         The values to be color-mapped.
 
     X, Y : array-like, optional
-        The coordinates of the corners of quadrilaterals of a pcolor.  
-        See `pcolor` for details. 
+        The coordinates of the corners of quadrilaterals of a pcolor.
+        See `pcolor` for details.
 
     cmap : str or `~matplotlib.colors.Colormap`, optional
         The Colormap instance or registered colormap name used to map
-        scalar data to colors. 
+        scalar data to colors.
         Defaults to :rc:`image.cmap`.
 
     vmin, vmax : scalar, optional
@@ -289,26 +291,26 @@ def wpcolor(*args,
         data. *vmin*, *vmax* are ignored if the *norm* parameter is used.
 
     vmid: scalar, optional
-        The data value that the middle of the colormap is warped to. 
-        For instance, if the colormap is the 'jet' colourmap, so that 
-        the middle of the map is green, then in the new colourmap, 
+        The data value that the middle of the colormap is warped to.
+        For instance, if the colormap is the 'jet' colourmap, so that
+        the middle of the map is green, then in the new colourmap,
         green corresponds to the value vmid.
-        Default is the middle of the range of the data or (*vmin* + *vmax*)/2 
+        Default is the middle of the range of the data or (*vmin* + *vmax*)/2
         if they are specified.
 
     beta:  scalar > 0, optional
-        Beta controls the rate of change of colours close to *vmid*.  
+        Beta controls the rate of change of colours close to *vmid*.
         Larger values of beta give a more rapid change of colour with
-        data value.  Values of beta in the range 1 to 5 are usual 
-        as they give more resolution to the data values close to *vmid*.  
+        data value.  Values of beta in the range 1 to 5 are usual
+        as they give more resolution to the data values close to *vmid*.
         beta < 1 compresses the colours close to *vmid*.
-    
+
     Nentries: int, optional
         Number of entries in the warped colourmap.
         Default: 256
 
     ax: matplotlib.axes.Axes, optional
-        The Matplotlib axes in which to plot. 
+        The Matplotlib axes in which to plot.
         Default:  The current axes.
 
     Returns
